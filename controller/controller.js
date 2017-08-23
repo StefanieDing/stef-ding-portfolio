f5cf25b2067230024d24a7621c3d4f808d365694
Stefanie Ding <StefanieDing@StefaniDingsMBP.home>
2017-08-22 22:27:40 -0400

    update links and portfolio section

--- controller/controller.js
+++ controller/controller.js
@@ -4,4 +4,16 @@ var router = express.Router();
 router.get('/', function(req, res){
   res.render('index');
 });
+
+router.get('/websites', function(req, res){
+  res.render('websites');
+});
+
+router.get('/animation', function(req, res){
+  res.render('animation');
+});
+
+router.get('/character', function(req, res){
+  res.render('character');
+});
 module.exports = router;
