eeb4e15f183bb65cdb44a8f2b7553af70c64e80d
Stefanie Ding <StefanieDing@StefaniDingsMBP.home>
2017-08-22 23:11:48 -0400

    deleted extra pages and their corresponding routes

--- controller/controller.js
+++ controller/controller.js
@@ -5,15 +5,4 @@ router.get('/', function(req, res){
   res.render('index');
 });
 
-router.get('/websites', function(req, res){
-  res.render('websites');
-});
-
-router.get('/animation', function(req, res){
-  res.render('animation');
-});
-
-router.get('/character', function(req, res){
-  res.render('character');
-});
 module.exports = router;
